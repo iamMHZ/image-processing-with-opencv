4484d6ae794c2435c292c8978a0c1ebbcd2a7714
Mohammad Hossein <iammhz77@gmail.com>
2019-09-06 11:51:31 +0430

    face + landmarks -> detected

--- detection/faceDetectionWithDlib/BlinkingDetection.py
+++ detection/faceDetectionWithDlib/BlinkingDetection.py
@@ -1,18 +1,41 @@
 import cv2
 import dlib
+import logging
+
+
+def draw_all_landmarks(frame, landmarks_predictor, gray_frame, face):
+    landmarks = landmarks_predictor(gray_frame, face)
+    for i in range(0, 68):
+        x = landmarks.part(i).x
+        y = landmarks.part(i).y
+        cv2.circle(frame, (x, y), 5, (150, 250, 80), -1)
+        # logging.info("landmark : " + str(i) + "location :  " + str(x) + "," + str(y))
 
 
 def main():
     cap = cv2.VideoCapture(0)
 
     face_detector = dlib.get_frontal_face_detector()
-    landmark_predictor = dlib.shape_predictor()
+    landmarks_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
     while cap.isOpened():
         _, frame = cap.read()
 
-        cv2.imshow("original", frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        faces = face_detector(gray)
+
+        for face in faces:
+            # print(face)
+            x1 = face.left()
+            y1 = face.top()
+            x2 = face.right()
+            y2 = face.bottom()
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (100, 200, 0), 2)
+
+            draw_all_landmarks(frame, landmarks_predictor, gray, face)
+
+        cv2.imshow("original", frame)
         key = cv2.waitKey(1)
         if key == 32:
             cv2.destroyAllWindows()
