2debbb0cd341b95fd2d5d8ea380cd84a6bc0bfb3
Mohammad Hossein <iammhz77@gmail.com>
2019-09-07 14:20:36 +0430

    drawing line for blinking detection

--- detection/faceDetectionWithDlib/BlinkingDetection.py
+++ detection/faceDetectionWithDlib/BlinkingDetection.py
@@ -1,9 +1,13 @@
 import cv2
 import dlib
 import logging
+import numpy as np
+
+face_detector = dlib.get_frontal_face_detector()
+landmarks_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 
-def draw_all_landmarks(frame, landmarks_predictor, gray_frame, face):
+def draw_all_landmarks(frame, gray_frame, face):
     landmarks = landmarks_predictor(gray_frame, face)
     for i in range(0, 68):
         x = landmarks.part(i).x
@@ -12,28 +16,39 @@ def draw_all_landmarks(frame, landmarks_predictor, gray_frame, face):
         # logging.info("landmark : " + str(i) + "location :  " + str(x) + "," + str(y))
 
 
+def detect_blinking(original_frame, gray_frame, face):
+    landmarks = landmarks_predictor(gray_frame, face)
+
+    landmark36 = landmarks.part(36).x, landmarks.part(36).y
+    landmark39 = landmarks.part(39).x, landmarks.part(36).y
+
+    cv2.line(original_frame, landmark36, landmark39, (0, 0, 255))
+
+
+def draw_face_rectangle(frame, face):
+    x1 = face.left()
+    y1 = face.top()
+    x2 = face.right()
+    y2 = face.bottom()
+    cv2.rectangle(frame, (x1, y1), (x2, y2), (100, 200, 0), 2)
+
+
 def main():
     cap = cv2.VideoCapture(0)
-
-    face_detector = dlib.get_frontal_face_detector()
-    landmarks_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
     while cap.isOpened():
         _, frame = cap.read()
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = face_detector(gray)
+        faces = face_detector(gray_frame)
 
         for face in faces:
             # print(face)
-            x1 = face.left()
-            y1 = face.top()
-            x2 = face.right()
-            y2 = face.bottom()
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (100, 200, 0), 2)
+            # draw_face_rectangle(frame, face)
 
-            draw_all_landmarks(frame, landmarks_predictor, gray, face)
+            # draw_all_landmarks(frame, gray_frame, face)
+            detect_blinking(frame, gray_frame, face)
 
         cv2.imshow("original", frame)
         key = cv2.waitKey(1)
@@ -43,4 +58,5 @@ def main():
             break
 
 
-main()
+if __name__ == "__main__":
+    main()
